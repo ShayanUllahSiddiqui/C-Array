9481774546079acd891dbf13fa2ed4f238d783ec
Macbook <macbook@ShayanMac.local>
2021-01-05 12:56:42 +0500

    new commit

--- Q1.c
+++ Q1.c
@@ -1,12 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int i;
-    i=1;
-    while(i<=100)
-{
-    printf("%d\n",i);
-    i=i+5;
-}
-    return 0;
+	int Array[50], i, j, temp, Size;
+	
+	printf("\nPlease Enter the Number of elements in an array  :  ");
+	scanf("%d", &Size);
+	
+	printf("\nPlease Enter %d elements of an Array \n", Size);
+	for (i = 0; i < Size; i++)
+	{
+		scanf("%d", &Array[i]);
+    }     
+	for (i = 0; i < Size; i++)
+	{
+		for (j = i + 1; j < Size; j++)
+		{
+			if(Array[i] > Array[j])
+			{
+				temp = Array[i];
+				Array[i] = Array[j];
+				Array[j] = temp;
+			}
+			
+		}
+	}
+	printf("\n **** Array of Elements in Ascending Order are : ****\n");
+	for (i = 0; i < Size; i++)
+	{
+		printf("%d\t", Array[i]);
+	}
+	
+	return 0;
 }
--- Q2.c
+++ Q2.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 
+#define MAX_SIZE 100  
+
 int main()
 {
-    int i, j, N;
+    int arr[MAX_SIZE];
+    int size, i, toSearch, found;
 
-    printf("Enter Number: ");
-    scanf("%d", &N);
+    printf("Enter size of array: ");
+    scanf("%d", &size);
 
-    for(i=1; i<=N; i++)
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
     {
-        for(j=1; j<=i; j++)
-        {
-            printf("%d", i);
-        }
+        scanf("%d", &arr[i]);
+    }
 
-        printf("\n");
+    printf("\nEnter element to search: ");
+    scanf("%d", &toSearch);
+
+    found = 0; 
+    
+    for(i=0; i<size; i++)
+    {
+        if(arr[i] == toSearch)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 1)
+    {
+        printf("\n%d is found at position %d\n", toSearch, i + 0);
+    }
+    else
+    {
+        printf("\n%d is not found in the array\n", toSearch);
     }
 
     return 0;
